1acdf42fac419d51dead7e109479cda698218154
Tyler <mccartty@mail.gvsu.edu>
2017-09-13 21:33:58 -0700

    Add barebones oauth callback

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./config/keys.js");
+
 const app = express();
 
-// default route
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// Google oauth2 callback
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    // this scope defines access to account resources
+    scope: ["profile", "email"]
+  })
+);
 
 // dynamic port binding for heroku, et.al.
 const PORT = process.env.PORT || 5000;
